0027284f92f04be396c23836d339ece30e66659d
galh <galh@sensus.co.il>
2017-12-24 15:07:55 +0200

    Add project files.

---
+++ todoList/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace todoList.Controllers
+{
+    public class TaskController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "12", "13", "14" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "22";
+        }
+
+
+        public void AddTask(TaskObj task)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+
+    public class TaskObj
+    {
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public string Date { get; set; }
+        public int Id { get; set; }
+    }
+}
