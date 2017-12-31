d10b10a2ccec2cbd8895ea35ceb091077164dfa2
galh <galh@sensus.co.il>
2017-12-31 10:17:30 +0200

    todo list

--- todoList/Controllers/ValuesController.cs
+++ todoList/Controllers/ValuesController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using todoList.Models;
 
 namespace todoList.Controllers
 {
     public class TaskController : ApiController
     {
+
         // GET api/values
         public IEnumerable<string> Get()
         {
@@ -24,6 +26,18 @@ namespace todoList.Controllers
 
         public void AddTask(TaskObj task)
         {
+            Task newTask = new Task
+            {
+                TaskId = task.Id,
+                Title = task.Title,
+                Details = task.Details,
+                Date = task.Date   
+            };
+            using (var db = new TodoListEntities1())
+            {
+                db.Tasks.Add(newTask);
+                db.SaveChanges();
+            }
         }
 
         // PUT api/values/5
@@ -42,7 +56,7 @@ namespace todoList.Controllers
     {
         public string Title { get; set; }
         public string Details { get; set; }
-        public string Date { get; set; }
-        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public long Id { get; set; }
     }
 }
