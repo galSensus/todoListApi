49593b56edff1ff4bf491b9b2890c597f018c484
galh <galh@sensus.co.il>
2018-01-05 20:15:39 +0200

    add edit and get work

---
+++ todoList/Controllers/TaskController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using todoList.Models;
+
+namespace todoList.Controllers
+{
+    public class TaskController : ApiController
+    {
+        [Route("api/task/addTask")]
+        [HttpPost]
+        public void AddTask(TaskObj task)
+        {
+            Task newTask = new Task
+            {
+                Title = task.Title,
+                Details = task.Details,
+                Date = task.Date
+            };
+            using (var db = new TodoListEntities1())
+            {
+                db.Tasks.Add(newTask);
+                db.SaveChanges();
+            }
+        }
+
+        [Route("api/task/editTask")]
+        [HttpPut]
+        public void EditTask(TaskObj task)
+        {
+            using (var db = new TodoListEntities1())
+            {
+                var selectedTask = db.Tasks.FirstOrDefault(x => x.TaskId == task.TaskId);
+                selectedTask = task;
+                db.Tasks.AddOrUpdate(selectedTask);
+                db.SaveChanges();
+            }
+        }
+
+        [Route("api/task/deleteTask")]
+        [HttpPost]
+        public void DeleteTask(long taskId)
+        {
+            using (var db = new TodoListEntities1())
+            {
+                var deleteTask = db.Tasks.FirstOrDefault(x => x.TaskId == taskId);
+                db.Tasks.Remove(deleteTask);
+                db.SaveChanges();
+            }
+        }
+
+        [Route("api/task/getTasks")]
+        [HttpGet]
+        public List<Task> GetTasks()
+        {
+            using (var db = new TodoListEntities1())
+            {
+                List<Task> tasksList = db.Tasks.ToList();
+                return tasksList;
+            }
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //// GET api/values
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "12", "13", "14" };
+        //}
+
+        //// GET api/values/5
+        //public string Get(int id)
+        //{
+        //    return "22";
+        //}
+
+
+
+
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+
+
+    public class TaskObj
+    {
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime Date { get; set; }
+        public long TaskId { get; set; }
+    }
+}
--- todoList/Models/Task.cs
+++ todoList/Models/Task.cs
@@ -11,12 +11,25 @@ namespace todoList.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using todoList.Controllers;
+
     public partial class Task
     {
         public long TaskId { get; set; }
         public string Title { get; set; }
         public string Details { get; set; }
         public Nullable<System.DateTime> Date { get; set; }
+
+        public static implicit operator Task(TaskObj taskObj)
+        {
+            Task newTask = new Task
+            {
+                TaskId = taskObj.TaskId,
+                Details = taskObj.Details,
+                Date = taskObj.Date,
+                Title = taskObj.Title
+            };
+            return newTask;
+        }
     }
 }
